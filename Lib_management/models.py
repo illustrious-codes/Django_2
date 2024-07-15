55e1f4131f9a1b510910f9632a26468f8134e6ea
illustrious-codes <onatayodavid105@gmail.com>
2024-07-14 18:15:58 -0700

    week2

--- Lib_management/models.py
+++ Lib_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class Author(models.Model):
@@ -11,7 +12,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    published_date = models.DateField()
+    published_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.title
--- Lib_management/views.py
+++ Lib_management/views.py
@@ -4,8 +4,10 @@ from .models import Author, Book
 def index(request):
     authors = Author.objects.all()
     books = Book.objects.all()
+    published_date = Book.objects.all()
     context = {
         "authors": authors,
-        "books": books
+        "books": books,
+        "created_at": published_date
     }
     return render(request, 'index.html', context)
